25faa000509ede9c09deb8174c18c4ccc6f93005
URounder <eric.graul@gmail.com>
2021-07-14 19:13:23 -0400

    new files

---
+++ alien_colors.py
@@ -0,0 +1,8 @@
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print("You earned 5 points.")
+elif alien_color == 'yellow':
+    print("You just earned 7 points.")
+else:
+    print("You just earned 15 points!")
---
+++ counting.py
@@ -0,0 +1,8 @@
+our_number = []
+for number in range(1, 11):
+    our_number.append(number ** 3)
+
+print(min(our_number))
+print(max(our_number))
+
+print(our_number)
